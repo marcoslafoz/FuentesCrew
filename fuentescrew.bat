e0b73f2aff394319927bbbdd1637b805a35c460a
Marcos Lafoz <marcos.lafoz96@gmail.com>
2023-02-20 20:24:12 +0100

    20/02/2023 - 20:24

---
+++ fuentescrew.bat
@@ -0,0 +1,29 @@
+@echo off
+
+SET "url=https://github.com/marcoslafoz/FuentesCrew/raw/master/FuentesCrew-mods.zip"
+SET "output=.\FuentesCrew-mods.zip"
+
+echo - - - - - - - - - - - - - - - - - - - - - - - - - - 
+echo Descargando archivos, no cierres esta ventana
+echo - - - - - - - - - - - - - - - - - - - - - - - - - - 
+"Powershell.exe" "(New-Object System.Net.WebClient).DownloadFile(\"%url%\", \"%output%\")"
+echo Archivos descargados correctamente
+echo - - - - - - - - - - - - - - - - - - - - - - - - - - 
+echo Descomprimiento archivos
+echo - - - - - - - - - - - - - - - - - - - - - - - - - - 
+powershell -command "Expand-Archive FuentesCrew-mods.zip FuentesCrew-mods"
+echo - - - - - - - - - - - - - - - - - - - - - - - - - - 
+echo Actualizando mods de Fuentes Crew
+echo - - - - - - - - - - - - - - - - - - - - - - - - - - 
+copy FuentesCrew-mods\FuentesCrew-mods\*.jar %APPDATA%\.minecraft\mods\
+cd %APPDATA%\.minecraft\mods\
+echo - - - - - - - - - - - - - - - - - - - - - - - - - - 
+echo Mods instalados: 
+echo - - - - - - - - - - - - - - - - - - - - - - - - - - 
+dir /b
+echo - - - - - - - - - - - - - - - - - - - - - - - - - - 
+echo @author: marcoslafoz
+echo - - - - - - - - - - - - - - - - - - - - - - - - - - 
+:End
+pause
+Exit /B 0
